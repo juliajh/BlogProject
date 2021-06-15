7bb471687d5db082acb6ccccab353efb46ffa152
juhee <juliajuhee222@gmail.com>
2021-06-15 15:04:41 +0900

    blog

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-&=nxk@mxmz2$b841jd9*!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('Debug','True')!='False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
